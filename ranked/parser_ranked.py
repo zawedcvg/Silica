b4665f32fd1103ad0ef356024f641637e21f8a0d
zawedcvg <neeladri02@gmail.com>
2023-12-24 19:41:40 +0800

    fix commander parsing as per latest log version fixed few bugs in commander parsing and allowed file names to be passed as arguments

--- ranked/parser_ranked.py
+++ ranked/parser_ranked.py
@@ -1,4 +1,5 @@
 from enum import Enum
+import sys
 import math
 import cProfile
 import collections
@@ -11,13 +12,13 @@ class Factions(Enum):
     ALIEN = "Alien"
 
 class Modes(Enum):
-    SOL_VS_ALIEN = 0
-    CENTAURI_VS_ALIEN = 1
-    CENTAURI_VS_SOL = 2
-    CENTAURI_VS_CENTAURI = 3
-    SOL_VS_SOL = 4
-    ALIENS_VS_ALIEN = 5
-    CENTAURI_VS_SOL_VS_ALIEN = 6
+    SOL_VS_ALIEN = "HUMANS_VS_ALIENS"
+    # CENTAURI_VS_ALIEN = 1
+    CENTAURI_VS_SOL = "HUMANS_VS_HUMANS"
+    # CENTAURI_VS_CENTAURI = 3
+    # SOL_VS_SOL = 4
+    # ALIENS_VS_ALIEN = 5
+    CENTAURI_VS_SOL_VS_ALIEN = "HUMANS_VS_HUMANS_VS_ALIENS" 
 
 class Player:
     def __init__(self, player_id: int, player_name: str, faction_type: Factions):
@@ -49,7 +50,7 @@ class Player:
 
     def __str__(self):
         return f"name: {self.player_name}, id: {self.player_id}, faction_type: {self.faction_type.value}, \
-unit_kills: {self.unit_kill}, structure_kill: {self.structure_kill}, deaths = {self.death}"
+unit_kills: {self.unit_kill}, structure_kill: {self.structure_kill}, deaths = {self.death} self.winner = {self.winner} is_commander = {self.is_commander}"
 
 #TODO
 #to get thing, just to Factions(name).name?
@@ -61,6 +62,8 @@ JOINED_TEAM = "joined team"
 CHAT = "say"
 TEAM_CHAT = "say_team"
 ROUND_START = "World triggered \"Round_Start\""
+ROUND_END = "World triggered \"Round_Win\""
+END_TIME = ""
 
 
 def is_valid_faction_type(match_type: Modes, faction_type: Factions):
@@ -74,25 +77,44 @@ def is_valid_faction_type(match_type: Modes, faction_type: Factions):
 def get_current_match(all_lines):
     inverted_list = all_lines[::-1]
     for i, value in enumerate(inverted_list):
-        if value[25:].strip() == ROUND_START:
+        if value[25:54].strip() == ROUND_START:
             return all_lines[len(all_lines) - i - 1:]
 
-def get_match_info(match_details):
+def is_current_match_completed(match_info):
+    for i in match_info[::-1]:
+        if i[25:52].strip() == ROUND_END:
+            date_string = i[1:23].strip()
+            global END_TIME
+            END_TIME = datetime.strptime(date_string, "%m-%d-%Y - %H:%M:%S")
+            return True
+    return False
+
+def get_match_type(match_details):
     #TODO add method for getting the type
-    return Modes.CENTAURI_VS_SOL, [Factions.CENTAURI, Factions.SOL]
+    match_type_pattern = r'\(gametype "(.*?)"\)'
+    match_type_info = match_details[0][54:].strip()
+    match_type = re.search(match_type_pattern, match_type_info)
+    if match_type is None:
+        print("Incorrect format of match type")
+        exit()
+    game_mode = Modes(match_type.group(1))
+    if game_mode == Modes.CENTAURI_VS_SOL:
+        factions = [Factions.SOL, Factions.CENTAURI]
+    elif game_mode == Modes.CENTAURI_VS_SOL_VS_ALIEN:
+        factions = [Factions.SOL, Factions.CENTAURI, Factions.ALIEN]
+    elif game_mode == Modes.SOL_VS_ALIEN:
+        factions = [Factions.SOL, Factions.ALIEN]
+    return game_mode, factions
 
 def get_commanders(match_log_info, match_mode_info, all_players):
     #TODO complete the implementation once logging bug is fixed
     #get the person with the max time as commander
-    #make it so that it can deal with duplicate names.
-
-    # commander_joined_pattern = r'"(.*?)<(.*?)><(.*?)><(.*?)>" triggered "took_command" \(structure "(.*)"\) \(struct_team "(.*)"\)'
-    commander_joined_pattern = r'Promoted <color=#7070FF>(.*?)<color=#DDE98C> to commander for <color=#7070FF>(.*?)"'
-    commander_left_pattern = r'left command'
+    commander_joined_pattern = r'"(.*?)<(.*?)><(.*?)><(.*?)>" changed role to "Commander"'
+    commander_left_pattern = r'"(.*?)<(.*?)><(.*?)><(.*?)>" changed role to "Infantry"'
     #mode, factions = match_mode_info incase i add more checks
     all_commanders = collections.defaultdict(list)
     commander_durations = collections.defaultdict(list)
-    commander_log_info = filter(lambda x: "command" in x, match_log_info)
+    commander_log_info = filter(lambda x: "changed" in x, match_log_info)
     #TODO, fix to set, else leave it as is.
     for i in commander_log_info:
         joined_match = re.search(commander_joined_pattern, i)
@@ -100,32 +122,35 @@ def get_commanders(match_log_info, match_mode_info, all_players):
         if joined_match:
             date_string = i[1:23].strip()
             start_time = datetime.strptime(date_string, "%m-%d-%Y - %H:%M:%S")
-            commander = joined_match.group(1)
-            faction_type = joined_match.group(2)
-            #TODOm change to id
-            all_commanders[Factions(faction_type).name].append(all_players[commander])
-            commander_durations[commander].append(start_time)
+            commander = int(joined_match.group(3))
+            faction_type = Factions(joined_match.group(4))
+            player = all_players[(commander, faction_type)]
+            #all_commanders is the all the factions and all the commanders they have had
+            all_commanders[faction_type].append(player)
+            commander_durations[player].append(start_time)
+        #change this once logging mod updates
         elif left_match:
+            commander = int(left_match.group(3))
+            faction_type = Factions(left_match.group(4))
+            player = all_players[(commander, faction_type)]
+            if player not in all_commanders[faction_type]:
+                continue
             date_string = i[1:23].strip()
             end_time = datetime.strptime(date_string, "%m-%d-%Y - %H:%M:%S")
-            commander = left_match.group(1)
-            faction_type = left_match.group(2)
-            commander_durations[commander].append(end_time)
-    end_time = ""
+            commander_durations[player].append(end_time)
     for duration in commander_durations.values():
-        if len(duration) % 2 == 0:
-            duration.append(end_time)
-    all_factions_match = list(commander_durations.keys())
+        if len(duration) % 2 != 0:
+            duration.append(END_TIME)
     final_commander = {}
-    for faction, commanders in all_factions_match:
+    for faction, commanders in all_commanders.items():
         max_duration = -1
         faction_commander = None
         for commander in commanders:
             commander_duration = commander_durations[commander]
-            commander_duration = sum([commander_duration[i + 1] - commander_duration[i]
+            total_commander_duration = sum([(commander_duration[i + 1] - commander_duration[i]).total_seconds()
                        for i in range(0, len(commander_duration), 2)])
-            if commander_duration > max_duration:
-                max_duration = commander_duration
+            if total_commander_duration > max_duration:
+                max_duration = total_commander_duration
                 faction_commander = commander
         final_commander[faction] = faction_commander
 
@@ -187,15 +212,16 @@ def process_unit_kills(all_match_info, all_players):
     for i in unit_kill_info:
         match = re.search(unit_kill_pattern, i.strip())
         if match:
-            player_id = int(match.group(3))
+            player_id = (match.group(3))
             #TODO add victim things
             player_faction = match.group(4)
             enemy_name = match.group(5)
-            enemy_id = int(match.group(7))
+            enemy_id = match.group(7)
             enemy_faction = match.group(8)
             if enemy_id != "":
-                all_players[(enemy_id, Factions(enemy_faction))].update_death()
-            all_players[(player_id, Factions(player_faction))].update_unit_kill(enemy_name)#change/fix this
+                all_players[(int(enemy_id), Factions(enemy_faction))].update_death()
+            if player_id != "":
+                all_players[(int(player_id), Factions(player_faction))].update_unit_kill(enemy_name)#change/fix this
 
 def probability(rating1, rating2):
     return 1.0 * 1.0 / (1 + 1.0 * math.pow(10, 1.0 * (rating1 - rating2) / 400))
@@ -205,6 +231,7 @@ def elo_rating_fps(Ra, Rb, K, d):
     #calculate based on points. use maybe an exponential thing?
     pass
 
+#https://www.geeksforgeeks.org/elo-rating-algorithm/
 def elo_rating_commander(Ra, Rb, K, d):
     # To calculate the Winning
     # Probability of Player B
@@ -230,22 +257,28 @@ def elo_rating_commander(Ra, Rb, K, d):
     print("Ra =", round(Ra, 6), " Rb =", round(Rb, 6))
 
 #reading the file
-def checking():
-    file_pointer = open("./test_file", "r")
+#TODO add flag to parse the entire log as for testing/some purpose
+
+def checking(file_name):
+    file_pointer = open(file_name, "r")
     all_lines = file_pointer.readlines()
-    team = get_winning_team(all_lines)
-    # print(team)
-    # all_players_in_game = {}
-    # match_log_info = get_current_match(all_lines)
-    # match_mode_info = get_match_info(match_log_info)
-    # all_essential_info = list(filter(remove_chat_messages, all_lines))
-    # all_players = get_all_players(all_essential_info)
-    # # get_commanders(all_lines, match_mode_info, all_players)
-    # all_essential_info = [remove_date_data(line) for line in all_essential_info]
-    # process_structure_kills(all_essential_info, all_players)
-    # process_unit_kills(all_essential_info, all_players)
-    # for i in all_players.values():
-        # print(i)
-        # # input()
-    # file_pointer.close()
+    match_log_info = get_current_match(all_lines)
+    is_complete = is_current_match_completed(match_log_info)
+    if not is_complete:
+        print("Aborting parsing, last match has incomplete information")
+        exit()
+    all_essential_info = list(filter(remove_chat_messages, match_log_info))
+    winning_team = get_winning_team(all_essential_info)
+    all_essential_info = [remove_date_data(line) for line in all_essential_info]
+    all_players = get_all_players(all_essential_info, winning_team)
+    process_structure_kills(all_essential_info, all_players)
+    process_unit_kills(all_essential_info, all_players)
+    get_commanders(match_log_info, None, all_players)
+    for i in all_players.values():
+        print(i)
+
+if __name__ == "__main__":
+    checking(sys.argv[1])
+
+# checking()
 # cProfile.run('checking()')
