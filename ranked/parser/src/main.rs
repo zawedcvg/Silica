cef88b6ddf659fe8ea2f118a416f08c1c5784d03
zawedcvg <neeladri02@gmail.com>
2024-07-09 17:25:20 +0800

    fix stuff for cross compile

--- ranked/parser/src/main.rs
+++ ranked/parser/src/main.rs
@@ -1,40 +1,96 @@
 pub mod inserting_info;
 use std::fs::OpenOptions;
-use std::os::unix::io::AsRawFd;
+use std::io::{self, Write};
 pub mod parser;
 use crate::inserting_info::inserting_info;
 use crate::parser::checking_folder;
 use std::env;
 
-fn main() {
+#[cfg(unix)]
+use {
+    std::os::unix::io::AsRawFd,
+};
 
+#[cfg(windows)]
+use {
+    std::os::windows::io::AsRawHandle,
+    winapi::um::processenv::SetStdHandle,
+    winapi::um::winbase::{STD_OUTPUT_HANDLE, STD_ERROR_HANDLE},
+    winapi::um::handleapi::INVALID_HANDLE_VALUE,
+};
+
+fn main() {
+    // Open files to append stdout and stderr
     let stdout_file = OpenOptions::new()
         .create(true)
         .append(true)
         .open("stdout.txt")
-        .unwrap_or_else(|e| panic!("unable to create stdout file due to {e}"));
-
+        .unwrap_or_else(|err| panic!("Failed to open stdout.txt: {}", err));
     let stderr_file = OpenOptions::new()
         .create(true)
         .append(true)
         .open("stderr.txt")
-        .unwrap_or_else(|e| panic!("unable to create stdout file due to {e}"));
+        .unwrap_or_else(|err| panic!("Failed to open stderr.txt: {}", err));
 
-    // Redirect stdout dont really know what's going on
-    unsafe {
-        let stdout_fd = libc::dup(1);
-        libc::dup2(stdout_file.as_raw_fd(), 1);
-        libc::close(stdout_fd);
+    #[cfg(unix)]
+    {
+        // Redirect stdout on Unix
+        unsafe {
+            let stdout_fd = libc::dup(1);
+            if stdout_fd == -1 {
+                panic!("Failed to duplicate stdout: {}", io::Error::last_os_error());
+            }
+            if libc::dup2(stdout_file.as_raw_fd(), 1) == -1 {
+                libc::dup2(stdout_fd, 1);
+                libc::close(stdout_fd);
+                panic!("Failed to redirect stdout: {}", io::Error::last_os_error());
+            }
+            libc::close(stdout_fd);
+        }
+
+        // Redirect stderr on Unix
+        unsafe {
+            let stderr_fd = libc::dup(2);
+            if stderr_fd == -1 {
+                panic!("Failed to duplicate stderr: {}", io::Error::last_os_error());
+            }
+            if libc::dup2(stderr_file.as_raw_fd(), 2) == -1 {
+                libc::dup2(stderr_fd, 2);
+                libc::close(stderr_fd);
+                panic!("Failed to redirect stderr: {}", io::Error::last_os_error());
+            }
+            libc::close(stderr_fd);
+        }
     }
 
-    // Redirect stderr
-    unsafe {
-        let stderr_fd = libc::dup(2);
-        libc::dup2(stderr_file.as_raw_fd(), 2);
-        libc::close(stderr_fd);
-    }
+    #[cfg(windows)]
+    {
+        use std::ptr::null_mut;
 
+        // Redirect stdout on Windows
+        unsafe {
+            let stdout_handle = stdout_file.as_raw_handle();
+            if stdout_handle == INVALID_HANDLE_VALUE as _ {
+                panic!("Invalid handle for stdout.txt: {}", io::Error::last_os_error());
+            }
+            if SetStdHandle(STD_OUTPUT_HANDLE, stdout_handle as _) == 0 {
+                panic!("Failed to set stdout handle: {}", io::Error::last_os_error());
+            }
+        }
+
+        // Redirect stderr on Windows
+        unsafe {
+            let stderr_handle = stderr_file.as_raw_handle();
+            if stderr_handle == INVALID_HANDLE_VALUE as _{
+                panic!("Invalid handle for stderr.txt: {}", io::Error::last_os_error());
+            }
+            if SetStdHandle(STD_ERROR_HANDLE, stderr_handle as _) == 0 {
+                panic!("Failed to set stderr handle: {}", io::Error::last_os_error());
+            }
+        }
+    }
     let args: Vec<String> = env::args().collect();
     let game = checking_folder(&args[1]);
     let _ = inserting_info(game);
+
 }
