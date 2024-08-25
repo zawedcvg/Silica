98b9419252b0b513ebbbf753abefd8337c24e90f
zawedcvg <neeladri02@gmail.com>
2024-08-24 20:32:56 -0400

    improve the logging, fix bugs, add tests and improve performance move to flexi-logger for logging. fix issue of incorrect calculation of commander elo for 3 ways. Add basic testing. Make the requests for commander elo batched to improve performance.

--- ranked/parser/src/main.rs
+++ ranked/parser/src/main.rs
@@ -1,25 +1,14 @@
 pub mod inserting_info;
 use dotenv::dotenv;
+use flexi_logger::{FileSpec, Logger, WriteMode};
+use log::{debug, error, info};
 use sqlx::postgres::PgPoolOptions;
-use std::fs::OpenOptions;
-use std::io::{self};
 use std::path::Path;
 use std::sync::Arc;
 pub mod parser;
 use crate::inserting_info::inserting_info;
 use crate::parser::checking_folder;
 use std::env;
-
-#[cfg(unix)]
-use std::os::unix::io::AsRawFd;
-
-#[cfg(windows)]
-use {
-    std::os::windows::io::AsRawHandle,
-    winapi::um::handleapi::INVALID_HANDLE_VALUE,
-    winapi::um::processenv::SetStdHandle,
-    winapi::um::winbase::{STD_ERROR_HANDLE, STD_OUTPUT_HANDLE},
-};
 
 #[tokio::main]
 async fn main() {
@@ -30,100 +19,50 @@ async fn main() {
 
     match env::set_current_dir(env_path) {
         Ok(_) => (),
-        Err(e) => panic!(
-            "Could not set current directory to {:?} due to {e}.",
-            env_path
-        ),
+        Err(e) => {
+            error!(
+                "Could not set current directory to {:?} due to {e}.",
+                env_path
+            );
+            panic!()
+        }
     }
 
     dotenv().ok();
 
-    let stdout_file = OpenOptions::new()
-        .create(true)
-        .append(true)
-        .open("stdout.txt")
-        .unwrap_or_else(|err| panic!("Failed to open stdout.txt: {}", err));
+    let separator =
+        "-------------------------------------------------------------------------------";
 
-    let stderr_file = OpenOptions::new()
-        .create(true)
-        .append(true)
-        .open("stderr.txt")
-        .unwrap_or_else(|err| panic!("Failed to open stderr.txt: {}", err));
-
-    #[cfg(unix)]
-    {
-        // Redirect stdout on Unix
-        unsafe {
-            let stdout_fd = libc::dup(1);
-            if stdout_fd == -1 {
-                panic!("Failed to duplicate stdout: {}", io::Error::last_os_error());
-            }
-            if libc::dup2(stdout_file.as_raw_fd(), 1) == -1 {
-                libc::dup2(stdout_fd, 1);
-                libc::close(stdout_fd);
-                panic!("Failed to redirect stdout: {}", io::Error::last_os_error());
-            }
-            libc::close(stdout_fd);
-        }
-
-        // Redirect stderr on Unix
-        unsafe {
-            let stderr_fd = libc::dup(2);
-            if stderr_fd == -1 {
-                panic!("Failed to duplicate stderr: {}", io::Error::last_os_error());
-            }
-            if libc::dup2(stderr_file.as_raw_fd(), 2) == -1 {
-                libc::dup2(stderr_fd, 2);
-                libc::close(stderr_fd);
-                panic!("Failed to redirect stderr: {}", io::Error::last_os_error());
-            }
-            libc::close(stderr_fd);
-        }
-    }
-
-    #[cfg(windows)]
-    {
-        use std::ptr::null_mut;
-
-        // Redirect stdout on Windows
-        unsafe {
-            let stdout_handle = stdout_file.as_raw_handle();
-            if stdout_handle == INVALID_HANDLE_VALUE as _ {
-                panic!(
-                    "Invalid handle for stdout.txt: {}",
-                    io::Error::last_os_error()
-                );
-            }
-            if SetStdHandle(STD_OUTPUT_HANDLE, stdout_handle as _) == 0 {
-                panic!(
-                    "Failed to set stdout handle: {}",
-                    io::Error::last_os_error()
-                );
-            }
-        }
-
-        // Redirect stderr on Windows
-        unsafe {
-            let stderr_handle = stderr_file.as_raw_handle();
-            if stderr_handle == INVALID_HANDLE_VALUE as _ {
-                panic!(
-                    "Invalid handle for stderr.txt: {}",
-                    io::Error::last_os_error()
-                );
-            }
-            if SetStdHandle(STD_ERROR_HANDLE, stderr_handle as _) == 0 {
-                panic!(
-                    "Failed to set stderr handle: {}",
-                    io::Error::last_os_error()
-                );
-            }
-        }
-    }
+    let _logger = Logger::try_with_str("info")
+        .unwrap()
+        .use_utc()
+        .log_to_file(
+            FileSpec::default()
+                .directory("parser-logs")
+                .suppress_timestamp()
+                .suffix("log"), // Optional: Add a .log suffix to the file name
+                                //.discriminant(discriminant)
+                                //.discriminant(|ts| ts.format("%Y-%m-%d").to_string()) // File name includes only date
+        )
+        .append()
+        .write_mode(WriteMode::BufferAndFlush)
+        .format(|w, now, record| {
+            write!(
+                w,
+                "{} [{}] {}",
+                now.format("%Y-%m-%d %H:%M:%S"), // Date and time in the timestamp
+                record.level(),
+                &record.args()
+            )
+        })
+        .start()
+        .unwrap();
 
     // Get the database URL from the environment variable
     let database_url = env::var("DATABASE_URL").unwrap();
 
     // Create a connection pool
+    // WARN the .env path is wrong? check
     let pool = PgPoolOptions::new()
         .max_connections(10)
         .connect(&database_url);
@@ -131,9 +70,12 @@ async fn main() {
     let log_folder = Path::new(&args[1]);
 
     //TODO getting the current map method takes a lot of time due to next line of rev lines/or is
-    //it vector capacity issues? not sure
+
+    info!("parsing the folder");
 
     let game = checking_folder(log_folder);
+
+    debug!("{:#?}", game.get_match_length());
 
     let _ = inserting_info(
         Arc::new(game),
@@ -142,4 +84,5 @@ async fn main() {
         }),
     )
     .await;
+    info!("{separator}");
 }
